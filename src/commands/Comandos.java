4f673b7dff844ef9b506e37ba449ade245882122
ruan <ruan.cavcalcante505@gmail.com>
2025-04-12 15:36:48 -0300

    adicionando comentários

--- src/commands/Comandos.java
+++ src/commands/Comandos.java
@@ -9,12 +9,13 @@ public class Comandos {
     public void add(String descricao) throws IOException {
         File file = new File("tasks.json");
 
-        // verifica se existe um JSON
+        // verifica se existe o tasks.json
         if (file.isFile() && file.exists()) {
             System.out.println(file + " Exists");
         } else {
-            System.out.println(file + " Do not exist! \nCreating one!");
 
+            // se não existe, cria
+            System.out.println(file + " Do not exist! \nCreating one!");
             file.createNewFile();
             try (FileWriter fileWriter = new FileWriter(file)) {
                 fileWriter.write("[]");
