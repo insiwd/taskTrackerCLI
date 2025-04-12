826bea02c3719ec191f812901f21e69b08672cd0
ruan <ruan.cavcalcante505@gmail.com>
2025-04-12 15:36:02 -0300

    verifica se tasks.json existe, se não existe, cria

--- src/commands/Comandos.java
+++ src/commands/Comandos.java
@@ -1,20 +1,25 @@
 package commands;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Comandos {
 
-    public void add(String descricao) {
+    public void add(String descricao) throws IOException {
         File file = new File("tasks.json");
-        
+
         // verifica se existe um JSON
-        if (file.isFile()) {
+        if (file.isFile() && file.exists()) {
             System.out.println(file + " Exists");
-        }
-        else {
+        } else {
             System.out.println(file + " Do not exist! \nCreating one!");
 
+            file.createNewFile();
+            try (FileWriter fileWriter = new FileWriter(file)) {
+                fileWriter.write("[]");
+                fileWriter.flush();
+            }
         }
     }
-
 }
