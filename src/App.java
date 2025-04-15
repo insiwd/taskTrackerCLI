cbc4d76102f468019c2a233b27915098df50c29d
ruan <ruan.cavcalcante505@gmail.com>
2025-04-15 14:15:13 -0300

    verifica se o JSON está vazio ou tem conteúdo

--- src/App.java
+++ src/App.java
@@ -13,3 +13,4 @@ public class App {
         System.out.println(comando);
 
     }
+}
--- src/commands/Comandos.java
+++ src/commands/Comandos.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import task.Task;
 
 public class Comandos {
 
@@ -14,6 +15,13 @@ public class Comandos {
         // verifica se existe o tasks.json
         if (arquivo.isFile() && arquivo.exists()) {
             System.out.println(arquivo + " Existe!");
+            Task task = new Task(descricao);
+            String taskJson = String.format(
+                "{\"id\": %d, \"descricao\": %s}",
+                task.getId(),
+                task.getDescription()
+            );
+
         } else {
             // se não existe, cria
             System.out.println(arquivo + " Não existe :( \nCriando..!");
@@ -26,11 +34,22 @@ public class Comandos {
 
         // precisa de um try-with-resources para não ter warning
         try (Scanner sc = new Scanner(arquivo)){
+            // guardando o JSON emuma string
+            StringBuilder conteudo = new StringBuilder();
+            // lendo e guardando
             while (sc.hasNextLine()) {
-                String data = sc.nextLine();
-                System.out.println(data);
+                conteudo.append(sc.nextLine());
             }
-            sc.close();
+
+            // guardando o JSON lido em outra string, para ver se está vazio ou tem conteúdo
+            // removendo espaços
+            // se está vazio, printa que está vazio!!
+            String jsonBruto = conteudo.toString().replaceAll("\\s+", "");
+            if (jsonBruto.equals("[]")) {
+                System.out.println("Vazio!");
+            } else {
+                System.out.println("Tem conteúdo!");
+            }
         } catch (FileNotFoundException e) {
             System.out.println("Ocorreu um erro: " + e);
         }
